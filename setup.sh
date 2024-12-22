692569d0cc5d83810736438ef0e282a68bbb9e6c
btakahira <braian.takahira@gmail.com>
2024-12-22 12:42:13 -0300

    fixed requirements

--- setup.sh
+++ setup.sh
@@ -12,8 +12,14 @@ pip install -r requirements.txt
 # create a local .env file with the specified default values
 cat <<EOL > .env
 YOUTUBE_API_KEY=<Your_Youtube_API_Key>
-ASSEMBLY_AI_API_KEYS=<Your_Assembly_AI_key>
+YOUTUBE_CHANNELS=<Channel1>,<Channel2>,...
+YOUTUBE_PLAYLISTS=<Playlist1>,<Playlist2>,...
+DOWNLOAD_VIDEO=True
+SERVICE_ACCOUNT_FILE=
 EOL
 
 # give user a notice
 echo "A .env file has been created. Please update its values with your API key, channels, and playlists."
+
+# deactivate virtual environment
+deactivate
